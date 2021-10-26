7605f5d98870d186d6b93cd75ee4575e4301bccf
osbenz <65363715+osbenz@users.noreply.github.com>
2021-10-26 21:59:46 +0100

    deployment changes

--- covid_visualization/settings.py
+++ covid_visualization/settings.py
@@ -118,9 +118,6 @@ import os
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'staticfiles'
-"""STATIC_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)"""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
